05c3125e47bf4a9e1c2d20c8553cbe312a8d349a
Nicolas <nicolas.mansilla@sansano.usm.cl>
2023-10-12 01:24:38 -0300

    agregada hora de chile

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const button = document.getElementById("myButton");
     const message = document.getElementById("message");
+    const chileTimeElement = document.getElementById("chileTime");
     var n = 0;
 
     button.addEventListener("click", function () {
@@ -12,4 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
             n=0;
         }
     });
+    
+    function updateChileTime() {
+        const chileTime = new Date().toLocaleString("en-US", { timeZone: "America/Santiago" });
+        chileTimeElement.textContent = chileTime;
+    }
+    updateChileTime();
+
+    // Actualizar la hora cada segundo
+    setInterval(updateChileTime, 1000);
 });
